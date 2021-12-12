500ba8e9d7bd4fea82bcf92c2287f2b2dfaa260a
farisbegic <faris.begic@stu.ssst.edu.ba>
2021-12-12 19:58:15 +0100

    Created list of products for each category

---
+++ frontend/data/products.js
@@ -0,0 +1,127 @@
+export const products = [
+    {
+        slug: "dioptrijske-naocale",
+        list: [
+            {
+                brand: "Sover",
+                model: "SO4028-54-BGD",
+                image: ""
+            },
+            {
+                brand: "Sover",
+                model: "SO4028-54-BRN",
+                image: ""
+            },
+            {
+                brand: "Sover",
+                model: "SO4058-51-BL",
+                image: ""
+            },
+            {
+                brand: "Sover",
+                model: "SO4058-51-BL",
+                image: ""
+            },
+        ]
+    },
+    {
+        slug: "suncane-naocale",
+        list: [
+            {
+                brand: "Polar",
+                model: "Polar Lemon",
+                image: ""
+            },
+            {
+                brand: "Polar",
+                model: "Polar Tudaio",
+                image: ""
+            },
+            {
+                brand: "Polar",
+                model: "Polar 739-76/B-51",
+                image: ""
+            },
+            {
+                brand: "Polar",
+                model: "Polar 739-428-51",
+                image: ""
+            },
+        ]
+    },
+    {
+        slug: "djecije-naocale",
+        list: [
+            {
+                brand: "Avanglion",
+                model: "AV14740A",
+                image: ""
+            },
+            {
+                brand: "Avanglion",
+                model: "AV14740A",
+                image: ""
+            },
+            {
+                brand: "Avanglion",
+                model: "AV14774E",
+                image: ""
+            },
+            {
+                brand: "Avanglion",
+                model: "AV14770F",
+                image: ""
+            },
+        ]
+    },
+    {
+        slug: "kontaktna-sociva",
+        list: [
+            {
+                brand: "Only One",
+                model: "Eco One (1 komad)",
+                image: ""
+            },
+            {
+                brand: "Bausch + Lomb",
+                model: "Ultra (3 komada)",
+                image: ""
+            },
+            {
+                brand: "Bausch + Lomb",
+                model: "PureVision 2 (3 komada)",
+                image: ""
+            },
+            {
+                brand: "Air Optix",
+                model: "Hydra Glyde (3 komada)",
+                image: ""
+            },
+        ]
+    },
+    {
+        slug: "tecnosti-za-lece",
+        list: [
+            {
+                brand: "Only One",
+                model: "Otopina 100ml",
+                image: ""
+            },
+            {
+                brand: "Bausch + Lomb",
+                model: "Otopina 120ml",
+                image: ""
+            },
+            {
+                brand: "Only One",
+                model: "Otopina 360ml",
+                image: ""
+            },
+            {
+                brand: "Bausch + Lomb",
+                model: "Otopina 360ml",
+                image: ""
+            },
+        ]
+    }
+];
